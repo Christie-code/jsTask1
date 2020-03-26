4ae464f94b146a04d10c321dbaf003a4248b8cd0
Christie-code <62036667+Christie-code@users.noreply.github.com>
2020-03-26 08:43:13 +0100

    Added task.js file

---
+++ task.js
@@ -0,0 +1,11 @@
+const name = "Olaoye Christiana";
+const courses= ["HTML", "CSS", "JavaScript", "Design"];
+const data= [];
+console.log(name);
+console.log(courses);
+for (let i=1; i<=200; i++) {
+  if (i%2 ===0) {
+    data.push(i);
+  }
+}
+console.log(data);
